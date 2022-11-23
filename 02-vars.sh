f28b3b5664a4cf13aaa8e3e8693c4ea9b5515f38
Venu <vennugopal.challa369@gmail.com>
2022-11-23 11:59:26 +0530

    Mysql Script

---
+++ 02-vars.sh
@@ -0,0 +1,18 @@
+## Variable - Variables are nothing but,  If we assign a name to s set of data is called as varaible
+
+# Declare a variable
+
+#VARNAME=DATA (No spaces)
+a=10
+
+#Access a variable-- $VARNAME ---> meaning $ symbol prefixing it
+
+echo A - $a
+
+#a=10 hardcoded manually, However we need the data dynamically .
+##Dynamically you can get in 2 ways
+#1. user Input
+#2.Script find out with other commands
+
+#command substitution
+#VARNAME=$(command)
