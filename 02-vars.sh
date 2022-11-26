d16eb0dd380071a122d82e4d1684cf14c9690d94
Venu <vennugopal.challa369@gmail.com>
2022-11-26 09:12:20 +0530

    Mysql Script

--- 02-vars.sh
+++ 02-vars.sh
@@ -5,7 +5,7 @@
 #VARNAME=DATA (No spaces)
 a=10
 
-#Access a variable-- $VARNAME ---> meaning $ symbol prefixing it
+#Access a variable-- ${VARNAME} ---> meaning $ symbol prefixing it
 
 echo A - $a
 
@@ -16,3 +16,26 @@ echo A - $a
 
 #command substitution
 #VARNAME=$(command)
+#VAR=$((expression))--->$((3=2))
+
+
+#VARiable names can have only characters a-z, A-Z 0-9, _(underscore) and special char are not allowedand should not start with a number
+#Var is not having any data types all are consider ass string
+
+#properties variable
+#Readonly VAR-->you cannot change the defined value
+#scalar a=(10,20)-->(array)--->access echo ${a[0]}--10
+#Loacal---export VAR
+
+
+
+#####################
+#Conditions ---case &
+              #if
+                 --simple if [ expression ]; then
+                   commands
+                   fi
+              #if-else--single condition
+              #elseif--multi condition
+
+                 fi
