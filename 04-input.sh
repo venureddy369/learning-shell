a86da708cc03caee0aa8eecf69dbf85d8f52cf75
Venu <vennugopal.challa369@gmail.com>
2022-11-26 20:39:08 +0530

    input commands

--- 04-input.sh
+++ 04-input.sh
@@ -1,2 +1,6 @@
-read -p 'Enter MySql Root password:' pass
-echo from read= $pass
+#read -s -p 'Enter MySql Root password:' pass
+#echo Password from read= $pass
+
+
+echo Username = $1
+echo Password = $2
